26955f6c342f3a4276cab04b8e2dee9ba0df395e
Richard Viernes <viernes@live.ca>
2021-03-20 14:51:23 -0400

    Added PyPoll.py

---
+++ PyPoll.py
@@ -0,0 +1,22 @@
+# Data needed to retrieve.
+# 1. Total number of votes casted
+# 2. Complete list of candidates who received votes
+# 3. Percentage of votes each candidate won
+# 4. Total number of votes each canidate won
+# 5. Winner of election based on popular vote
+
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
